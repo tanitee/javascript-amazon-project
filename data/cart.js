0840f34bac2d115f3856bf562f7c9c9ac3d44473
Tanitoluwa <tanitee13@gmail.com>
2024-09-11 12:05:39 +0100

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,16 @@
-export let cart = [{
-    productId:                                                
-        "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:2,                                                                                                
-}, {
-    productId :
-        "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:1,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'))
+    if(!cart){
+        cart = [{
+            productId:                                                
+                "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity:2,                                                                                                
+        }, {
+            productId :
+                "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity:1,
+        }];
+    }
+
 
 
 export function addToCart(productId){
@@ -26,6 +30,11 @@ export function addToCart(productId){
         });
     }
 
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
 }
 
 export function removeFromCart(productId){
@@ -34,10 +43,10 @@ export function removeFromCart(productId){
     cart.forEach((cartItem)=>{
         if(cartItem.productId !== productId){
             newCart.push(cartItem);
-            
         }
     });
 
     cart = newCart;
 
+    saveToStorage();
 }
