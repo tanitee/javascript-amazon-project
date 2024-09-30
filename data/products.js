bd099693972896b16dfd195881c26491132bc2e1
Tanitoluwa <tanitee13@gmail.com>
2024-09-30 11:55:51 +0100

    used inheritance an polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -32,7 +32,26 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`
   }
+  extraInfoHTML(){
+    return ``;
+  }
 }
+
+class clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+    `;
+  }
+}
+
 
 
 export const products = [
@@ -695,6 +714,9 @@ export const products = [
     ]
   }
 ].map((productDetails) =>{
+  if(productDetails.type === 'clothing'){
+    return new clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
